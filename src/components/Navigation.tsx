1468b397926bb89d9371f6db04cec3d71be295fc
Yosinxon422 <yosinxonsalimov442@gmail.com>
2025-03-30 13:21:53 +0300

    react

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Home, Users, FolderOpen, Settings } from "lucide-react";
 import "../styles/Navigation.css";
